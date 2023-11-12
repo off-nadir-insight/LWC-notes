2bbd6402ddd3fd364e6f4ef11e832a4d667ac186
Tyler <77578259+off-nadir-insight@users.noreply.github.com>
2023-11-12 07:29:58 -0800

    Add files via upload

---
+++ myLWC.js
@@ -0,0 +1,6 @@
+import { LightningElement, api } from 'lwc';
+export default class MyLWC extends LightningElement {
+	@api textBox = "Hello, World!?";
+    @api happy = false;
+    @api textColor = "Blue";
+}
