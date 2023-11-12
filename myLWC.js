5e507be997b0ae2a48be1450ddc942a05081c0e6
Tyler <77578259+off-nadir-insight@users.noreply.github.com>
2023-11-12 07:31:18 -0800

    Update myLWC.js tab spacing

--- myLWC.js
+++ myLWC.js
@@ -1,6 +1,6 @@
 import { LightningElement, api } from 'lwc';
 export default class MyLWC extends LightningElement {
 	@api textBox = "Hello, World!?";
-    @api happy = false;
-    @api textColor = "Blue";
+    	@api happy = false;
+    	@api textColor = "Blue";
 }
