6ec9fd4074727c7db392e3148c1063466d9e7a3b
Tyler <77578259+off-nadir-insight@users.noreply.github.com>
2023-11-18 07:27:39 -0800

    Adds methods to combine parameters

--- myLWC.js
+++ myLWC.js
@@ -1,6 +1,28 @@
 import { LightningElement, api } from 'lwc';
 export default class MyLWC extends LightningElement {
 	@api textBox = "Hello, World!?";
-    	@api happy = false;
-    	@api textColor = "Blue";
+	@api boldText = false;
+	@api textColor = "Blue";
+	@api borderWidth = 0;
+	@api myStyle = "";
+	@api text1 = "a";
+	@api text2 = "b";
+	@api combinedText;
+
+    get combinedText() {
+        return `${this.text1} ${this.text2}`;
+    }
+    get combinedStyle() {
+        let returnString = "";
+        if (this.boldText == true) {
+            returnString += 'font-weight: bold;'
+        }
+        if (this.textColor) {
+            returnString += `color:${this.textColor};`
+        }
+        if (this.borderWidth) {
+            returnString += `border:${this.borderWidth}px solid black;`
+        }
+        return returnString;
+    }
 }
